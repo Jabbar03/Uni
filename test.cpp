c6ccb9f4a5c9c7bae566a7732d2b66cfb415003a
Omar Ayache <102006902+Jabbar03@users.noreply.github.com>
2022-10-22 23:47:51 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,68 +1,76 @@
 #include <iostream>
 using namespace std;
-const int len=10;
+const int len=20;
+int age_tot=0;
+const int n_stud=3;
 
-struct queue{
-	int data[len];
-	int testa;
-	int num_el;
+//struct studente
+struct student {
+  char name[len];
+  char surname[len];
+  int sum_mark;
+  int n_mark;
 };
 
-queue new_queue() {
-	queue tmp;
-	Q.testa=0;
-	Q.num_el=0;
-	return(tmp);
+//funzione nome
+student name(student tmp) {
+  int i=0;
+  bool flag=false;
+  cout << "Inserire il nome dello studente, inserire 0 al termine: " << "\n";
+  while(!flag) {
+    cin >> tmp.name[i];
+    if(tmp.name[i]=='0') flag=true;
+    i++;
+  }
+  flag=false;  i=0;
+  cout << "Inserire il cognome dello studente, inserire 0 al termine: " << "\n";
+  while(!flag) {
+    cin >> tmp.name[i];
+    if(tmp.name[i]=='0') flag=true;
+    i++;
+  }
+  return(tmp);
 }
 
-bool isempty (queue Q) {
-	if(Q.num_el=0) return(true);
-	return(false);
+//inserimento studente: voti ed età
+student new_stud() {
+  student tmp;
+  int age;
+  tmp.n_mark=0;
+  tmp.sum_mark=0;
+  cout << "Inserire l'eta' dello studente: " << endl;
+  cin >> age;
+  age_tot += age;
+  tmp = name(tmp);
+  return(tmp);
 }
 
-queue enqueue (queue Q, int e) {
-	if(Q.num_el<len) {
-		Q.data[(Q.testa+Q.num:el)%len]=e;
-		Q.num_el++;
-	return(Q);
+//funzione nuovo voto
+student new_mark(student S) {
+  int m;
+  bool flag=true;
+  cout << "Inserire il voto dell'esame (fra 0 e 30):  " << "\n";
+    cin >> m;
+    if((m>=0)&&(m<=30)) {
+      S.n_mark++;
+      S.sum_mark += m;}
+  return(S);
 }
 
-queue dequeue (queue Q) {
-	Q.testa=(Q.testa%len)+1;
-	Q.num_el--;
-	return(Q);
+//funzione media voti
+double avg_mark(student S) {
+  return(S.sum_mark/double(S.n_mark));
 }
 
-queue print_queue(queue Q) {
-	for(int i=Q.num_el; i<Q.num_el; i++) {
-		cout << Q.data[Q.num_el];
-	}
+//funzione media età studenti
+double avg_age(student S) {
+  return(age_tot/double(n_stud));
 }
-
-
 
 int main() {
-queue Q;
-int e;
-	Q = new_queue();
-	for(int i = 0; i <= 8; i++) enqueue(Q, i;);
-	print_queue();
-	dequeue(Q);
-	dequeue(Q);
-	Q = enqueue(Q, 9);
-	Q = enqueue(Q, 10);
-	Q = enqueue(Q, 11);
-	dequeue(Q);
-	print_queue(Q);
-	Q = enqueue(Q, 12);
-	Q = enqueue(Q, 13);
-	dequeue(Q);
-	print_queue(Q);
+  student A = new_stud();
+  new_mark(A);
+  new_mark(A);
+  new_mark(A);
+  cout << avg_mark(A) << endl;
 }
-
-
-
-
-
-
-
