1c2459a4d20f36cdc2a4f3757ddadda86f32f93b
Omar Ayache <102006902+Jabbar03@users.noreply.github.com>
2022-10-23 14:57:20 +0200

    All fixed

--- es_studenti.cpp
+++ es_studenti.cpp
@@ -19,15 +19,14 @@ student name(student tmp) {
   cout << "Inserire il nome dello studente, inserire 0 al termine: " << "\n";
   while(!flag) {
     cin >> tmp.name[i];
-    if(tmp.name[i]==0) flag=true;
+    if(tmp.name[i]=='0') flag=true;
     i++;
   }
-  cout << "Nome inserito: " << tmp.name[0] << endl;
   flag=false;  i=0;
+  cout << "Inserire il cognome dello studente, inserire 0 al termine: " << "\n";
   while(!flag) {
-    cout << "Inserire il cognome dello studente, inserire 0 al termine: " << "\n";
-    cin >> tmp.surname[i];
-    if(tmp.surname[i]==0) flag=true;
+    cin >> tmp.name[i];
+    if(tmp.name[i]=='0') flag=true;
     i++;
   }
   return(tmp);
@@ -47,28 +46,44 @@ student new_stud() {
 }
 
 //funzione nuovo voto
-student new_mark(student S, int m) {
-  bool flag=true;
-  cout << "Inserire il voto dell'esame, il voto può andare da 0 a 30, inserire un altro numero non funziona:  " << "\n";
-    cin >> m;
-    if((m>=0)&&(m<=30)) S.sum_mark += m;
+student new_mark(student& S) {
+  int m;
+  cout << "Inserire il voto dell'esame (fra 0 e 30):  " << "\n";
+  cin >> m;
+  if((m>=0)&&(m<=30)) {
+    S.n_mark++;
+    S.sum_mark += m;
+  }
   return(S);
 }
 
 //funzione media voti
-double avg_mark(student S) {
-  return(S.sum_mark/S.n_mark);
+double avg_mark(student& S) {
+  return(S.sum_mark/double(S.n_mark));
 }
 
 //funzione media età studenti
-double avg_age(student S) {
+double avg_age() {
   return(age_tot/double(n_stud));
 }
 
 int main() {
   student A = new_stud();
-  new_mark(A, 25);
-  new_mark(A, 22);
-  new_mark(A, 20);
-  cout << avg_mark(A) << endl;
+  new_mark(A);
+  new_mark(A);
+  new_mark(A);
+  cout << "Media voti studente A: " << avg_mark(A) << endl;
+  new_mark(A);
+  cout << "Media voti studente A: " << avg_mark(A) << endl;
+  student B = new_stud();
+  new_mark(B);
+  new_mark(B);
+  new_mark(B);
+  cout << "Media voti studente B: " << avg_mark(B) << endl;
+  student C = new_stud();
+  new_mark(C);
+  new_mark(C);
+  new_mark(C);
+  cout << "Media voti studente C: " << avg_mark(C) << endl;
+  cout << "Media eta' degli studenti: " << avg_age() << endl;
 }
